3e15fee30d5dc2a3446ded5d7bbcdd2cc7a24e2a
Prince Nyarko <nyankonton122@gmail.com>
2023-12-03 17:29:27 -0300

    mo

--- app.py
+++ app.py
@@ -80,7 +80,8 @@ def mapa():
     m = folium.Map(local,zoom_start = 12)
 
     m._height = '100%'
-
+    viewport = folium.Viewport(width='device-width', height='device-height', zoom_control=False)
+    viewport.add_to(m)
     # Trecho do banco de dados da ANATEL, contendo as coordenadas das 4 principais estações de TV de Belém
     # Link: http://sistemas.anatel.gov.br/se/public/view/b/srd.php
     # Exemplo de dado armazenado: {"Nome da emissora":"lat,long,pot,ganho"}
@@ -230,7 +231,6 @@ def mapa():
         color:red;
         font-size:large
     }
-    hight:100%;
     </style>
     """))
 
